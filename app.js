86dca52496349c28db4f07c5e7ea10843affacc3
jihazard <yoonjh238@gmail.com>
2019-11-04 15:14:13 +0900

    writeDtm 기준 sort

--- app.js
+++ app.js
@@ -5,13 +5,23 @@ var testRouter = require("./index")
 var woo = require("./base/woo")
 var line = require("./base/line")
 var mo1 = require("./mo1")
+var mo2 = require("./mo2")
 var crwalingRouter = require("./crwaling_blogs")
 
+
 app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/123', function(req, res){
-  mo1.then(function(redata){
-     return res.json(redata.sort(function(a,b){return a.index-b.index}))
-  })
+app.get('/123', async function(req, res){
+    var x = await mo1 
+    console.log("x ==>  " + x.length)
+    var y =await mo2
+    console.log("y ==>  " + y.length)
+   
+   var t = x.concat(y);
+  console.log("====t ====> " + t.length)
+    return res.json(t.sort(function(a,b){
+        a = new Date(a.writeDtm);
+        b = new Date(b.writeDtm);
+      return a>b ?-1:a<b ?1:0;}))
 })
 app.listen(port, () => console.log(`Example app listening on port port!`))
 
