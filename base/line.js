14fb638cea3b280b1b6a356a7e02ef9ac7f73ce0
jihazard <yoonjh238@gmail.com>
2019-11-04 14:15:21 +0900

    line update

--- base/line.js
+++ base/line.js
@@ -12,52 +12,58 @@ const sanitizeHtml = require('sanitize-html');
 
    
     promise.then((result)=>{
-        //console.log(resolveData)
-        return res.json(result)
+      // console.log(resolveData)
+   
+        return res.json( result.sort(function(a,b){return a.index-b.index}))
     })
 });
 
 //Promise 테스트 완료
 
     let promise = new Promise(function(resolve, reject){
-        const URL  = "http://woowabros.github.io/";
+
+
+        const URL  = "https://engineering.linecorp.com/ko/blog/page/";
         let requestOptions = { method: "GET" ,uri: URL, headers: { "User-Agent": "Mozilla/5.0" } ,encoding: null};
        
         let resultArr = [];
-        request(requestOptions, function(error, response, body){
-            iconv.extendNodeEncodings(); 
-            var strContents = new Buffer(body); 
-            
-          const $ = cheerio.load(iconv.decode(strContents, 'UTF-8').toString());
-    
-          $(".list-module").each((i,el)=>{
-                let info = new Object();
-                const date = $(el).find(".post-meta").text().replace(/\s\s+/g, '');
-                const writeDtm = new Date(Date.parse(date.substr(0,13))).toLocaleDateString();
-                const writer = date.substr(date.length-3,date.length).trim();
-                const avatar = $(el).find(".post-meta").find("img").attr("src")
-                const title = $(el).find("a").find("h2").text().replace(/\s\s+/g, '');
-                const description = $(el).find("a").find("p").text().replace(/\s\s+/g, '');
-                const url = URL + $(el).find("a").attr("href")
-                info.writer = writer;
-                info.writeDtm = writeDtm;
-                info.avatar = avatar;
-                info.title = title;
-                info.description = description;
-                info.url = url;
-                resultArr.push(info)
-
+        for(let itx = 1; itx <= 18; itx++){
+            let url = URL +itx;
+            console.log("url ==> " + url)
+            request(url, function(error, response, body){
+                iconv.extendNodeEncodings(); 
+                var strContents = new Buffer(body); 
                 
-          })
-          console.log("result ===> " + resultArr.length)
-          resolve(resultArr)
-          if(resultArr > 0) {
-              console.log("========")
-              resolve(resultArr)
-          }
+              const $ = cheerio.load(iconv.decode(strContents, 'UTF-8').toString());
+ 
+              $("article").each((i,el)=>{
+                    let info = new Object(); 
+                    const header = $(el).find("h2").text().replace(/\s\s+/g, '');
+                    const index = itx;
+                    const title = $(el).find("h2").text().replace(/\s\s+/g, '');
+                    const writeDtm = $(el).find(".byline").text().replace('|', '').trim();
+                    const writer = $(el).find(".author-name").text().replace(/\s\s+/g, '').split("|")[0];
+                    const avatar  = $(el).find(".avatar").attr("src");
+                    const description  = $(el).find(".entry-summary p").text();
+                    const url  = $(el).find(".plain").attr("href");
 
-         
-        })
+                    info.index = index;
+                    info.writer = writer;
+                    info.writeDtm = writeDtm;
+                    info.avatar = avatar;
+                    info.title = title;
+                    info.description = description;
+                    info.url = url;
+                   
+                    resultArr.push(info)
+                    
+              })
+
+              resolve(resultArr) 
+             
+            })
+        }
+ 
     })
    
 
